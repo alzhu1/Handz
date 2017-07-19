037e064d9e31f77abfb52bb18cc0fea6e1a250dd
William Zhu <william21101@gmail.com>
2017-07-19 15:18:34 -0700

    add http2 support with Twh Twisted

--- mysite/settings.py
+++ mysite/settings.py
@@ -108,13 +108,24 @@ STATIC_URL = 'http://storage.googleapis.com/gcs_bucket_handz/static/'
 STATIC_ROOT = 'static/'
 
 
-
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'asgi_redis.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [('handz-redis.dmkkt6.ng.0001.usw1.cache.amazonaws.com', 6379)],
-        },
-        'ROUTING': 'deal.routing.channel_routing',
+if 'RDS_HOSTNAME' in os.environ:
+    CHANNEL_LAYERS = {
+        'default': {
+            'BACKEND': 'asgi_redis.RedisChannelLayer',
+            'CONFIG': {
+                'hosts': [('handz-redis.dmkkt6.ng.0001.usw1.cache.amazonaws.com', 6379)],
+            },
+            'ROUTING': 'deal.routing.channel_routing',
+        }
     }
-}
+else:
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "asgi_redis.RedisChannelLayer",
+            "CONFIG": {
+                'hosts': [('localhost', 6379)],
+#                "hosts": [("redis://:theworldisquiethere@pub-redis-10306.us-west-2-1.1.ec2.garantiadata.com:10306/0")],
+            },
+            "ROUTING": "deal.routing.channel_routing",
+        },
+    }
