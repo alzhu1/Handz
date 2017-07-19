b1ff2db04bd232c3555d128032ed3bd624d53860
William Zhu <william21101@gmail.com>
2017-07-18 18:26:58 -0700

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -108,23 +108,13 @@ STATIC_URL = 'http://storage.googleapis.com/gcs_bucket_handz/static/'
 STATIC_ROOT = 'static/'
 
 
-if 'RDS_HOSTNAME' in os.environ:
-    CHANNEL_LAYERS = {
-        'default': {
-            'BACKEND': 'asgi_redis.RedisChannelLayer',
-            'CONFIG': {
-                'hosts': [('handz-redis.dmkkt6.ng.0001.usw1.cache.amazonaws.com', 6379)],
-            },
-            'ROUTING': 'deal.routing.channel_routing',
-        }
-    }
-else:
-    CHANNEL_LAYERS = {
-        "default": {
-            "BACKEND": "asgi_redis.RedisChannelLayer",
-            "CONFIG": {
-                "hosts": [("redis://:theworldisquiethere@pub-redis-10306.us-west-2-1.1.ec2.garantiadata.com:10306/0")],
-            },
-            "ROUTING": "deal.routing.channel_routing",
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'asgi_redis.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('handz-redis.dmkkt6.ng.0001.usw1.cache.amazonaws.com', 6379)],
         },
+        'ROUTING': 'deal.routing.channel_routing',
     }
+}
