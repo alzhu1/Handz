8cb3d8f13982b2e984d7c12b7f4919703f25e22d
William Zhu <william21101@gmail.com>
2017-07-21 17:06:10 -0700

    db is causing redis problems?

--- mysite/settings.py
+++ mysite/settings.py
@@ -60,49 +60,28 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
-# if 'RDS_HOSTNAME' in os.environ:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
-
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "asgi_redis.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("redis://:theworldisquiethere@pub-redis-10306.us-west-2-1.1.ec2.garantiadata.com:10306/0")],
-        },
-        "ROUTING": "deal.routing.channel_routing",
-    },
-}
-
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'william',
-#             'USER': '',
-#             'PASSWORD': '',
-#             'HOST': 'localhost',
-#             'PORT': '5432',
-#         }
-#     }
-#
-#     CHANNEL_LAYERS = {
-#         "default": {
-#             "BACKEND": "asgi_redis.RedisChannelLayer",
-#             "CONFIG": {
-#                 'hosts': [('localhost', 6379)],
-#             },
-#             "ROUTING": "deal.routing.channel_routing",
-#         },
-#     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'william',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 # Internationalization
 
@@ -129,6 +108,15 @@ STATIC_URL = 'http://storage.googleapis.com/gcs_bucket_handz/static/'
 STATIC_ROOT = 'static/'
 
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "asgi_redis.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("redis://:theworldisquiethere@pub-redis-10306.us-west-2-1.1.ec2.garantiadata.com:10306/0")],
+        },
+        "ROUTING": "deal.routing.channel_routing",
+    },
+}
 
 # if 'RDS_HOSTNAME' in os.environ:
 #     CHANNEL_LAYERS = {
