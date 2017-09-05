89e60433c2dfb94290b089411c74070224ac287a
Alex Zhu <er.and.alex@gmail.com>
2017-09-05 10:34:21 -0700

    Project soft restart

--- channels_app/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'channels_app.apps.ChannelsAppConfig'
--- deal/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import BridgeTable, Deal, MyUser
-
-# Register your models here.
-admin.site.register(MyUser, UserAdmin)
-admin.site.register(BridgeTable)
-admin.site.register(Deal)
