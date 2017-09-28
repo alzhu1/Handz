5d94abf2b67229c7779ab54ab17c21ac52c96c64
William Zhu <william21101@gmail.com>
2017-09-28 14:30:33 -0700

    format main.js

--- frontend/src/containers/Main.js
+++ frontend/src/containers/Main.js
@@ -15,7 +15,8 @@ class Main extends React.Component {
     render() {
         return (
             <Switch>
-                <Route exact path='/' render={() => (this.props.token !== "" ?
+                <Route exact path='/'
+                render={() => (this.props.token !== "" ?
                     (<LobbyContainer  socket={this.props.lobbySock}/>) :
                     (<Redirect to='/login' />))} />
 
@@ -24,15 +25,22 @@ class Main extends React.Component {
 
 
                 <Route exact path='/signup' render={(props) => (
-                    <SignupContainer {...props} socket={this.props.signupSock} />
+                    <SignupContainer {...props}
+                    socket={this.props.signupSock} />
                 )} />
 
                 <Route exact path='/create-text' render={(props) => (
-                    <CreateText {...props} token={this.props.token} socket={this.props.lobbySock}  add_text={this.props.add_text} />
+                    <CreateText {...props}
+                    token={this.props.token}
+                    socket={this.props.lobbySock}
+                    add_text={this.props.add_text} />
                 )}  />
 
                 <Route exact path='/chat' render={(props) => (
-                    <Chat {...props} token={this.props.token} socket={this.props.chatSock} texts={this.props.texts} add_text={this.props.add_text}/>
+                    <Chat {...props} token={this.props.token}
+                    socket={this.props.chatSock}
+                    texts={this.props.texts}
+                    add_text={this.props.add_text}/>
                 )}  />
 
                 <Route render={() => (<Redirect to='/login' />)} />
