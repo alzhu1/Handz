64a86ba523351bc133b1ee32b844e0f65e6bc14e
William Zhu <william21101@gmail.com>
2018-05-04 14:08:40 -0700

    remove play buttom from tutorial

--- frontend/src/containers/tutorial/T12.js
+++ frontend/src/containers/tutorial/T12.js
@@ -5,26 +5,29 @@ import {mapStateToProps, mapDispatchToProps} from 'redux/map';
 import {connect} from 'react-redux';
 import Button from 'material-ui/Button';
 
-const T12= () => {
+class T12 extends React.Component {
 
-  const textStyle = {
 
+  // resetGame = () => {
+  //   this.props.createTableThunk('single')
+  //   this.handleClose()
+  // }
+
+  render() {
+    const textStyle = {
+
+    }
+    const title = 'You are now ready to play some Handz!'
+
+    return (
+      <div>
+        <Typography style={textStyle}>
+          {title}
+        </Typography>
+      </div>
+    )
   }
 
-
-  const title = 'You are now ready to play some Handz!'
-
-  return (
-    <div>
-      <Typography style={textStyle}>
-        {title}
-      </Typography>
-      <Button component={props => <Link to="/table" {...props} /> }
-          onClick={() => {this.props.createTableThunk('single')}}>
-            Play!
-      </Button>
-    </div>
-  )
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(T12);
