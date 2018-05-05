6cb7da6e437b2f67c36af6f25978e216cae70ec3
William Zhu <william21101@gmail.com>
2018-05-04 23:56:03 -0700

    update eb settings for redeployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -31,8 +31,9 @@ import red from 'material-ui/colors/red';
 const loggerMiddleware = createLogger();
 
 let sock = new WebSocketBridge();
+sock.connect('ws://backend-dev2.us-west-1.elasticbeanstalk.com/');
 // sock.connect('ws://handz-backend-dev.us-west-1.elasticbeanstalk.com');
-sock.connect('ws://localhost:8000');
+// sock.connect('ws://localhost:8000');
 sock.listen((payload, stream) => {
     store.dispatch(payload)
 });
